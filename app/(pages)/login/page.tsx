3ad9c87cf731d2ecd67bdd906c956c69b963b3b8
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-06 15:11:48 +0200

    Login Seite

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -3,9 +3,9 @@ import LoginForm from "@/app/ui/login_form";
 export default function LoginSeite() {
 
     return (
-        <main className="flex items-center justify-center md:h-screen">
-        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-          <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+        <main className="flex items-center justify-center ">
+        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-2 md:-mt-32">
+          <div className="flex h-20 w-full items-end p-3 md:h-36">
             <div className="w-32 text-white md:w-36">
               
             </div>
