3321720451287ae8e0be2258c3bdf8b01e51ec3f
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-06 15:17:12 +0200

    Anleitung erstellen auskommentiert

--- app/(pages)/Anleitungerstellen/page.tsx
+++ app/(pages)/Anleitungerstellen/page.tsx
@@ -1,8 +1,8 @@
-import { fetchUser } from "@/app/lib/data"
+// import { fetchUser } from "@/app/lib/data"
 
 export default async function Anleitungerstellen() {
 
-    const user = await fetchUser();
+    // const user = await fetchUser();
 
     return (
         <main>
