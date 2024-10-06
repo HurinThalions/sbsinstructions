499b0d80c804b004d0f3de6d317e634580300e5a
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-06 05:00:10 +0200

    Suche

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,25 @@ import { Suspense } from "react";
 
 import { KatalogSkeleton } from "./ui/skeletons";
 import Katalog from "./ui/katalog";
+import Suche from "@/app/ui/suche";
 
-export default function Start() {
+export default async function Start({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+
+  const query = searchParams?.query || '';
+  const currenPage = Number(searchParams?.page) || 1;
+
   return (
     <main>
-      <div className="mb-4 text-xl md:text-2xl">Suchleiste</div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Suche placeholder="Anleitungen durchsuchen"/>
+      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<KatalogSkeleton />}>
           <Katalog />
