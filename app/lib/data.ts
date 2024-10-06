e71ff768edffdd10ecd54f877b45e5da870501cc
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-06 04:24:26 +0200

    PPA & Anleitung fetch

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,11 +7,30 @@ export async function fetchAnleitungen() {
     try {
         const data = await sql<Anleitung>`
         SELECT * FROM anleitungen ORDER BY titel ASC
+        LIMIT 6
     `
     const anleitungen = data.rows;
     return anleitungen;
     } catch (error) {
         console.error('Datenbank fehler', error);
         throw new Error('Anleitungen holen fehlgeschlagen');
+    }
+}
+
+
+export async function fetchletzteAnleitung() {
+
+    try {
+        const data = await sql<Anleitung>`
+        SELECT anleitungen.id, anleitungen.titel, anleitungen.dauer, anleitungen.datum, anleitungen.bild
+        FROM anleitungen
+        ORDER BY anleitungen.datum DESC
+        LIMIT 1`;
+
+        const anleitung = data.rows;
+        return anleitung;
+    } catch (error) {
+        console.error('Datenbank fehler: ', error);
+        throw new Error('Fehler beim holen der letzten Anleitung');
     }
 }
