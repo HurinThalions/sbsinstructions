4ab56f6738140cd3d01a3a58bdd49505cc8fb456
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-06 10:33:40 +0200

    styling vom Katalog

--- app/ui/katalog.tsx
+++ app/ui/katalog.tsx
@@ -17,7 +17,7 @@ export default async function Katalog({
         <div className="flex flex-col m-20">
             <div className="overflow-hidden">
                 <h1 className="">Anleitungen</h1>
-                <table className="min-w-full text-center">
+                <table className={`min-w-full text-center ${styles.container}`}>
                     <thead className="border-b">
                         <tr>
                             <th>Titel</th>
@@ -28,11 +28,11 @@ export default async function Katalog({
                     </thead>
                     <tbody>
                         {anleitungen.map((anleitung) => (
-                            <tr key={anleitung.id} className="px-6 py-4 text-sm">
-                                <td className="whitespace-nowrap px-6 py-4 text-sm " >{anleitung.titel}</td>
-                                <td className="whitespace-nowrap px-6 py-4 text-sm">{anleitung.dauer}</td>
-                                <td className="whitespace-nowrap px-6 py-4 text-sm">{anleitung.datum}</td>
-                                <td className="whitespace-nowrap px-6 py-4 text-sm">
+                            <tr key={anleitung.id} className="">
+                                <td className=" px-6 py-4 text-sm " >{anleitung.titel}</td>
+                                <td className=" px-6 py-4 text-sm">{anleitung.dauer}</td>
+                                <td className="px-6 py-4 text-sm">{anleitung.datum}</td>
+                                <td className="px-6 py-4 text-sm">
                                     <Image 
                                         src={`${anleitung.bild}`} 
                                         alt={anleitung.titel} 
