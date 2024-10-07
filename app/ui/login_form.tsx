641abe9333019ea57f7a22b72036860dccbc1ce4
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-07 12:53:38 +0200

    deutsche sprache

--- app/ui/login_form.tsx
+++ app/ui/login_form.tsx
@@ -9,7 +9,7 @@ export default function LoginForm() {
         <form className="">
         <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
           <h1 className='mb-3 text-2xl'>
-            Please log in to continue.
+            Bitte melden Sie sich an.
           </h1>
           <div className="w-full">
             <div>
@@ -17,7 +17,7 @@ export default function LoginForm() {
                 className="mb-3 mt-5 block text-xs font-medium text-gray-900"
                 htmlFor="email"
               >
-                Email
+                E-Mail
               </label>
               <div className="relative">
                 <input
@@ -25,7 +25,7 @@ export default function LoginForm() {
                   id="email"
                   type="email"
                   name="email"
-                  placeholder="Enter your email address"
+                  placeholder="Bitte Ihre E-Mail eingeben"
                   required
                 />
                 <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -36,7 +36,7 @@ export default function LoginForm() {
                 className="mb-3 mt-5 block text-xs font-medium text-gray-900"
                 htmlFor="password"
               >
-                Password
+                Passwort
               </label>
               <div className="relative">
                 <input
@@ -44,7 +44,7 @@ export default function LoginForm() {
                   id="password"
                   type="password"
                   name="password"
-                  placeholder="Enter password"
+                  placeholder="Bitte Ihr Passwort eingeben"
                   required
                   minLength={6}
                 />
