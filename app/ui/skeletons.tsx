ce019ea4811d891a6b8789f27fcf8b3d21e9677f
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-10 10:27:24 +0200

    katalogskeleton

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,38 +1,11 @@
+import styles from '@/app/ui/css/katalog.module.css';
 
   export function KatalogSkeleton() {
     return (
-      <div
-        className={` relative flex w-full flex-col overflow-hidden md:col-span-4`}
-      >
-        <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
-        <div className="flex grow flex-col justify-between rounded-xl bg-gray-100 p-4">
-          <div className="bg-white px-6">
-            <EinzelanleitungSkeleton />
-            <EinzelanleitungSkeleton />
-            <EinzelanleitungSkeleton />
-            <EinzelanleitungSkeleton />
-            <EinzelanleitungSkeleton />
-          </div>
-          <div className="flex items-center pb-2 pt-6">
-            <div className="h-5 w-5 rounded-full bg-gray-200" />
-            <div className="ml-2 h-4 w-20 rounded-md bg-gray-200" />
-          </div>
+      <div className="flex flex-col place-items-center">
+        <div className={` ${styles.container}`}>
+
         </div>
       </div>
     );
-}
-
-export function EinzelanleitungSkeleton() {
-  return (
-    <div className="flex flex-row items-center justify-between border-b border-gray-100 py-4">
-      <div className="flex items-center">
-        <div className="mr-2 h-8 w-8 rounded-full bg-gray-200" />
-        <div className="min-w-0">
-          <div className="h-5 w-40 rounded-md bg-gray-200" />
-          <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
-        </div>
-      </div>
-      <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
-    </div>
-  );
 }
