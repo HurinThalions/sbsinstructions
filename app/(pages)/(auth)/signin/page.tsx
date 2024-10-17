a12d74f46ef84542442e45310fcbd05f3e963329
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-17 11:27:12 +0200

    Suspense für searchparams

--- app/(pages)/(auth)/signin/page.tsx
+++ app/(pages)/(auth)/signin/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import LoginForm from '@/app/ui/auth/login_form';
 import Popup from '@/app/ui/popup';
  
@@ -6,7 +8,9 @@ export default function LoginPage() {
     <main>
       <div className="relative rounded-lg drop-shadow mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
         <Popup />
-        <LoginForm />
+        <Suspense>
+          <LoginForm />
+        </Suspense>
       </div>
     </main>
   );
