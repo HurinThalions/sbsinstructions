f3e80132ce27780890c3d53e542aeeb06a0eda57
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-17 05:45:03 +0200

    middleware anpassungen

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
+console.log('Middleware geladen');
+
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { authConfig } from './app/auth.config';
 import { NextResponse, NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt'; 
 
@@ -9,11 +11,14 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({req, secret: process.env.NEXTAUTH_SECRET });
 
+  console.log('Token: ', token);
+
   if (!token) {
     const loginURL = new URL('/signin', req.url);
     return NextResponse.redirect(loginURL);
   }
 
+  console.log('Eingeloggt, zugriff gewährt');
   return NextResponse.next();
 }
 
