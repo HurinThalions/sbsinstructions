c6cb4ff98479bdebaf23877c025fb1ba5655f20c
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-17 09:20:34 +0200

    updates

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,19 @@
-import NextAuth from 'next-auth';
-import { authConfig } from '@/app/auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  console.log('Token: ', token);
+
+  if (!token) {
+    const loginURL = new URL('/signin', req.url);
+    return NextResponse.redirect(loginURL);
+  }
+  
+  return NextResponse.next();
+}
+
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/Anleitungerstellen'],
 };
