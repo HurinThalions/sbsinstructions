39199523ff0d3d8c4b1872bf1b7a186d6832f6a7
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-20 16:55:14 +0200

    Katalog styling

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default async function Start({
       <div className="mt-10 flex items-center justify-between gap-2 md:mt-8 m-10">
           <Suche placeholder="Anleitungen durchsuchen"/>
         </div>
-        <div className="w-full items-center justify-between">
+        <div className="w-full items-center justify-between p-2">
           <Suspense key={query + currentPage} fallback={<KatalogSkeleton />}>
             <KatalogServer query={query} currentPage={currentPage}></KatalogServer>
           </Suspense>
