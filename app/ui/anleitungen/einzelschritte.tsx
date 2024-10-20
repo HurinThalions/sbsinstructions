c65ad995d214f1352cf28ef34ec997cd8c7d9fdb
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-20 16:23:04 +0200

    Navigation Zurück zu Überblick & Anleitung beenden

--- app/ui/anleitungen/einzelschritte.tsx
+++ app/ui/anleitungen/einzelschritte.tsx
@@ -1,6 +1,7 @@
+'use client';
 import { useState } from "react";
 import Image from "next/image";
-
+import { useRouter } from 'next/navigation';  // Verwende useRouter für Navigation
 import { Progressbar } from "./progressbar";
 
 export default function Einzelschritte({
@@ -12,11 +13,12 @@ export default function Einzelschritte({
     beschreibung: string;
     bild: string;
     material: string;
+    anleitung_id: number;
   }>;
 }) {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
-
   const currentStep = schritte[currentStepIndex];
+  const router = useRouter();
 
   const handleNextStep = () => {
     if (currentStepIndex < schritte.length - 1) {
@@ -25,7 +27,9 @@ export default function Einzelschritte({
   };
 
   const handlePreviousStep = () => {
-    if (currentStepIndex > 0) {
+    if (currentStepIndex === 0) {
+      router.push(`/Anleitung/${currentStep.anleitung_id}`);
+    } else {
       setCurrentStepIndex(currentStepIndex - 1);
     }
   };
@@ -50,38 +54,40 @@ export default function Einzelschritte({
             src={`/${currentStep.bild}`}
             width={500}
             height={350}
-            alt="Logo"
+            alt="Bild"
             className="rounded-lg hidden lg:block"
           />
           <Image
             src={currentStep.bild}
             width={150}
             height={100}
-            alt="Logo"
+            alt="Bild"
             className="rounded-lg block lg:hidden"
           />
         </div>
       )}
 
       <div className="fixed bottom-10 w-full flex items-center justify-center px-20">
+        {/* Dynamischer Zurück-Button */}
         <button
           onClick={handlePreviousStep}
-          disabled={currentStepIndex === 0}
           className="btn btn-secondary whitespace-nowrap min-w-[10%] max-w-[40%] sm:max-w-[70%] md:max-w-xs bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
         >
-          Zurück
+          {currentStepIndex === 0 ? "Zurück zum Überblick" : "Zurück"}
         </button>
 
+        {/* Progressbar */}
         <div className="w-1/3">
           <Progressbar currentStepIndex={currentStepIndex} totalSteps={schritte.length} />
         </div>
 
+        {/* Dynamischer Nächster Schritt/Beenden-Button */}
         <button
           onClick={handleNextStep}
           disabled={currentStepIndex === schritte.length - 1}
           className="btn btn-primary whitespace-nowrap min-w-[10%] max-w-[40%] sm:max-w-[70%] md:max-w-xs bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
         >
-          Nächster Schritt
+          {currentStepIndex === schritte.length - 1 ? "Anleitung beenden" : "Nächster Schritt"}
         </button>
       </div>
     </>
