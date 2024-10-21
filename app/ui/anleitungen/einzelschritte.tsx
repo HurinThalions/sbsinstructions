50cb43ae3c70021008412eba8f6322f1e2d2395b
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-21 13:48:14 +0200

    weiterleitung zur Endseite

--- app/ui/anleitungen/einzelschritte.tsx
+++ app/ui/anleitungen/einzelschritte.tsx
@@ -23,6 +23,8 @@ export default function Einzelschritte({
   const handleNextStep = () => {
     if (currentStepIndex < schritte.length - 1) {
       setCurrentStepIndex(currentStepIndex + 1);
+    } else {
+      router.push('Endseite');
     }
   };
 
@@ -68,7 +70,6 @@ export default function Einzelschritte({
       )}
 
       <div className="fixed bottom-10 w-full flex items-center justify-center px-20">
-        {/* Dynamischer Zurück-Button */}
         <button
           onClick={handlePreviousStep}
           className="btn btn-secondary whitespace-nowrap min-w-[10%] max-w-[40%] sm:max-w-[70%] md:max-w-xs bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700"
@@ -76,12 +77,10 @@ export default function Einzelschritte({
           {currentStepIndex === 0 ? "Zurück zum Überblick" : "Zurück"}
         </button>
 
-        {/* Progressbar */}
         <div className="w-1/3">
           <Progressbar currentStepIndex={currentStepIndex} totalSteps={schritte.length} />
         </div>
 
-        {/* Dynamischer Nächster Schritt/Beenden-Button */}
         <button
           onClick={handleNextStep}
           disabled={currentStepIndex === schritte.length - 1}
