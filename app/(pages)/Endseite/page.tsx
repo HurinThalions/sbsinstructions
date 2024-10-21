8d88f270da6b8980f06d6b1a5c874e74c801489f
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-21 14:21:20 +0200

    Neue Bilder

--- app/(pages)/Endseite/page.tsx
+++ app/(pages)/Endseite/page.tsx
@@ -1,15 +1,26 @@
-import {  } from "@heroicons/react/16/solid";
+import { CheckIcon } from "@heroicons/react/16/solid";
+import Image from "next/image";
 
 import EndseitenButton from "@/app/ui/EndseiteButton";
+
 
 export default function Endseite() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
-        <div className="w-40 h-40 bg-green-500 rounded-full flex items-center justify-center">
-          <h1 className="text-white text-3xl font-bold">✔️</h1>
-        </div>
+        <Image
+            src={'/GruenerHaken.svg'}
+            height={300}
+            width={300}
+            alt="GruenerHaken"
+            className="hidden md:block"></Image>
+        <Image
+            src={'/GruenerHaken.svg'}
+            height={300}
+            width={300}
+            alt="GruenerHaken"
+            className="block md:hidden"></Image>
         <h1 className="text-2xl font-bold text-center mt-4">Aufgabe geschafft!</h1>
           <EndseitenButton />
       </div>
     );
-  }
+}
