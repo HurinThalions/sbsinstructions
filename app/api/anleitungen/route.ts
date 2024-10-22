4db11f27486ff18f4c99f27a61133aca03c5cf08
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-22 08:43:38 +0200

    Kommentare

--- app/api/anleitungen/route.ts
+++ app/api/anleitungen/route.ts
@@ -5,18 +5,15 @@ export async function POST(req: Request) {
   try {
     const { title, duration, date, image, user } = await req.json();
 
-    // Anleitung in die Datenbank einfügen und die ID der neu erstellten Anleitung zurückgeben
     const result = await sql`
       INSERT INTO anleitungen (title, duration, date, image, nutzername)
       VALUES (${title}, ${duration}, ${date}, ${image}, ${user})
       RETURNING id;
     `;
 
-    // Hole die ID der neu erstellten Anleitung
     const anleitungId = result.rows[0].id;
-
-    // Sende die ID in der Antwort zurück
     return NextResponse.json({ id: anleitungId, message: 'Anleitung erfolgreich erstellt' });
+    
   } catch (error) {
     console.error('Fehler beim Speichern der Anleitung:', error);
     return NextResponse.json({ error: 'Fehler beim Erstellen der Anleitung' }, { status: 500 });
