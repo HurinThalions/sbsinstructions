be695c1a13934de44db7d7daceaf74564650eaaf
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-24 16:28:40 +0200

    DESC

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -25,7 +25,7 @@ export async function fetchgefilterteAnleitungen(
             anleitungen.title ILIKE ${searchQuery} OR
             anleitungen.date::text ILIKE ${searchQuery} OR
             anleitungen.duration::text ILIKE ${searchQuery}
-        ORDER BY anleitungen.date ASC
+        ORDER BY anleitungen.date DESC
         LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
         `;
         
