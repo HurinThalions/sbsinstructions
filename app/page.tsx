0f63c7ad40d981c225c06ae89911f47b81a360e0
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-25 20:04:40 +0200

    Hover text

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,14 @@ export default async function StartPage({
         <div className="mt-10 flex items-center justify-between gap-2 md:mt-8 m-10">
           <Suche placeholder='Anleitungen durchsuchen' />
         </div>
-        <div className="w-full items-center justify-between p-2">
+        <div className="w-full items-center justify-between p-2 group">
           <Suspense key={query + currentPage} fallback={<KatalogSkeleton />}>
             <KatalogServer query={query} currentPage={currentPage} />
           </Suspense>
+          {/* Erklärung Anleitung */}
+          <p className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                Auf die Anleitung klicken, um die Anleitung zu öffnen
+            </p>
         </div>
       </div>
     </main>
--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -8,6 +8,7 @@ export default function Navbar() {
 
   return (
     <div className={styles.body}>
+        {/* Link zur Startseite */}
         <div className={styles.header}>
                 <a className={styles.logocontainer} href="/">
                     <Image
@@ -25,6 +26,7 @@ export default function Navbar() {
                         className="block md:hidden"
                     />
                 </a>
+            {/* Link zu Anleitung erstellen */}
         <div className={`${styles.boxumAErstellen} relative group`}>
             <Link href='/Anleitungerstellen'>
                 <PlusIcon
@@ -36,11 +38,12 @@ export default function Navbar() {
                     width={35}
                     height={35}/>
             </Link>
-                {/* Tooltip */}
-            <span className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                {/* Anzeige bei Hover */}
+            <p className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 Anleitung erstellen
-            </span>
+            </p>
         </div>
+        {/* Link zu Einloggen */}
         <div className={`${styles.boxumProfil} relative group`}>
             <Link href={'/signin'}>
                 <UserIcon
@@ -54,10 +57,10 @@ export default function Navbar() {
                     height={35}
                     />
             </Link>
-                {/* Tooltip */}
-            <span className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                {/* Anzeige bei Hover */}
+            <p className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 Anmeldung & Registrierung
-            </span>
+            </p>
         </div>
         </div>
     </div>
