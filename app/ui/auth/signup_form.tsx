56271fef1c545db7df11c442e64e3f4e6079708a
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-26 08:48:19 +0200

    router von next/navigation verwenden

--- app/ui/auth/signup_form.tsx
+++ app/ui/auth/signup_form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { AtSymbolIcon, KeyIcon, UserIcon, ExclamationCircleIcon, ArrowRightIcon } from '@heroicons/react/16/solid';
 import { SignupButton } from '../signinupbutton';
 
