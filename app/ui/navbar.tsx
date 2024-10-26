f018263e4ae32627dcba42efe29a27b1e46b9c74
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-26 02:47:17 +0200

    Link in box not img

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -27,41 +27,44 @@ export default function Navbar() {
                     />
                 </a>
             {/* Link zu Anleitung erstellen */}
-        <div className={`${styles.boxumAErstellen} relative group`}>
-            <Link href='/Anleitungerstellen'>
-                <PlusIcon
-                    className={`${styles.aErstellen} hidden md:block `}
-                    width={55}
-                    height={55}/>
-                <PlusIcon
-                    className={`${styles.aErstellen} block md:hidden`}
-                    width={35}
-                    height={35}/>
-            </Link>
-                {/* Anzeige bei Hover */}
-            <p className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <Link href='/Anleitungerstellen' className={`${styles.boxumAErstellen} group`}>
+            <div className="flex items-center justify-center w-full h-full">
+            <PlusIcon
+              className={`${styles.aErstellen} hidden md:block`}
+              width={55}
+              height={55}
+            />
+            <PlusIcon
+              className={`${styles.aErstellen} block md:hidden`}
+              width={35}
+              height={35}
+            />
+          </div>
+        {/* Anzeige bei Hover */}
+            <p className="absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 Anleitung erstellen
             </p>
-        </div>
+        </Link>
+
         {/* Link zu Einloggen */}
-        <div className={`${styles.boxumProfil} relative group`}>
-            <Link href={'/signin'}>
-                <UserIcon
-                    className={`${styles.profil} hidden md:block`}
-                    width={55}
-                    height={55}
-                    />
-                <UserIcon
-                    className={`${styles.profil} block md:hidden`}
-                    width={35}
-                    height={35}
-                    />
-            </Link>
-                {/* Anzeige bei Hover */}
-            <p className="absolute bottom-14 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <Link href='/signin' className={`${styles.boxumProfil} group`}>
+          <div className="flex items-center justify-center w-full h-full">
+            <UserIcon
+              className={`${styles.profil} hidden md:block`}
+              width={55}
+              height={55}
+            />
+            <UserIcon
+              className={`${styles.profil} block md:hidden`}
+              width={35}
+              height={35}
+            />
+          </div>
+            {/* Anzeige bei Hover */}
+            <p className="absolute bottom-2 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                 Anmeldung & Registrierung
             </p>
-        </div>
+        </Link>
         </div>
     </div>
   );
