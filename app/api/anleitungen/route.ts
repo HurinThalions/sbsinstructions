74a0879e818d81781d8911cf8d93c64ecb73b696
HurinThalions <tsvetkov.svet@gmail.com>
2024-10-27 16:33:45 +0100

    Katalog refresh funktioniert

--- app/api/anleitungen/route.ts
+++ app/api/anleitungen/route.ts
@@ -1,5 +1,6 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export async function POST(req: Request) {
   try {
@@ -18,5 +19,36 @@ export async function POST(req: Request) {
   } catch (error) {
     console.error('Fehler beim Speichern der Anleitung:', error);
     return NextResponse.json({ error: 'Fehler beim Erstellen der Anleitung' }, { status: 500 });
+  }
+}
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const query = searchParams.get('query') || '';
+  const page = Number(searchParams.get('page') || 1);
+  const offset = (page - 1) * 10;
+  const searchQuery = `%${query}%`;
+
+  try {
+    const anleitungen = await sql`
+      SELECT
+        id,
+        title,
+        duration,
+        date,
+        image
+      FROM anleitungen
+      WHERE
+        anleitungen.title ILIKE ${searchQuery} OR
+        anleitungen.date::text ILIKE ${searchQuery} OR
+        anleitungen.duration::text ILIKE ${searchQuery}
+      ORDER BY anleitungen.date DESC
+      LIMIT 10 OFFSET ${offset};
+    `;
+
+    return NextResponse.json(anleitungen.rows);
+  } catch (error) {
+    console.error('Datenbankfehler: ', error);
+    return NextResponse.json({ error: 'Fehler beim Abrufen der Anleitungen' }, { status: 500 });
   }
 }
