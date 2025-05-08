a97d23afebf248ba5b6f73826063c1bb7a1ed81f
HurinThalions <tsvetkov.svet@gmail.com>
2025-05-08 08:11:14 +0200

    swr

--- app/(pages)/Endseite/page.tsx
+++ app/(pages)/Endseite/page.tsx
@@ -5,6 +5,7 @@ import EndseitenButton from "@/app/ui/EndseiteButton";
 export default function Endseite() {
     return (
       <div className="flex items-center justify-center max-h-screen">
+        <div>
         <Image
             src={'/GruenerHaken.svg'}
             height={300}
@@ -18,6 +19,7 @@ export default function Endseite() {
             alt="GruenerHaken"
             className="block md:hidden"></Image>
           <EndseitenButton />
+          </div>
           <h1 className="text-2xl font-bold text-center mt-4 p-4">Aufgabe geschafft!</h1>
           </div>
     );
